336f2ec620a8a6df822660f206e1f2d6890e2733
Cooper Ren <cren2@hwemail.com>
2025-09-24 12:56:06 -0700

    Initializes a basic Git repository structure
    Creates the directory structure and necessary files
    for a basic Git repository. This includes the 'git'
    directory, the 'objects' subdirectory, and the 'index'
    and 'HEAD' files. It also checks if the directories and files already exist.

--- Git.java
+++ Git.java
@@ -1,5 +1,39 @@
+import java.io.File;
+
 public class Git {
     public static void main(String[] args) {
-        
+        createRepository();
+
+    }
+
+    public static void createRepository() {
+        File git = new File("git");
+        File objects = new File(git, "objects");
+        File index = new File(git, "index");
+        File head = new File(git, "HEAD");
+        if (git.exists() && objects.exists() && index.exists() && head.exists()) {
+            System.out.println("Git Repository Already Exists");
+        } else {
+            if (!git.exists()) {
+                git.mkdir();
+            }
+
+            if (!objects.exists()) {
+                objects.mkdir();
+            }
+            try {
+
+                if (!index.exists()) {
+                    index.createNewFile();
+                }
+
+                if (!head.exists()) {
+                    head.createNewFile();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("Git Repository Created");
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+Initializing the Repository:
+The method createRepository is the method that initializes the repository, "Git". It starts by checking whether all the files and directories necessary are present. If they are, it lets the user know they all exist. If they aren't, it creates the missing files or directories. The files and directories present are:
+
+git: the main directory
+objects: a directory in git
+HEAD: A file in git
+index: A file in git
+
+No difficulties with this part
