5d2021bac23374172d1ed139f44047627114453f
Cooper Ren <cren2@hwemail.com>
2025-09-30 08:46:00 -0700

    (GP-2.4.2)
    Implements a robust reset function to clean up the
    working directory and recreate the index.
    
    This ensures a clean state for testing and resolves
    potential issues caused by leftover files from
    previous test runs.

--- GitTester.java
+++ GitTester.java
@@ -1,13 +1,12 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class GitTester {
-    public static void main(String[] args) throws FileNotFoundException {
-        indexTest();
+    public static void main(String[] args) throws IOException {
+        robustReset();
 
     }
 
@@ -130,6 +129,33 @@ public class GitTester {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void robustReset() throws IOException {
+        File directory = new File(".");
+        resetAllFiles(directory);
+        File index = new File("git/index");
+        index.delete();
+        index.createNewFile();
+
+    }
+
+    public static void resetAllFiles(File directory) {
+        File[] list = directory.listFiles();
+        for (File f : list) {
+            if (!f.getName().equals("README.md") && f.getName().charAt(0) != '.'
+                    && !f.getName().contains(".java") && list != null && !f.getName().equals("HEAD")
+                    && !f.getName().equals("index")) {
+                if (f.isDirectory()) {
+                    resetAllFiles(f);
+
+                } else {
+                    f.delete();
+                }
+
+
+            }
         }
     }
 }
