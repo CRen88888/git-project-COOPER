296e4796521799746bb7157ad3c795f4465296b2
Cooper Ren <cren2@hwemail.com>
2025-10-06 23:44:26 -0700

    Tried my Best to Implement Git functionalities
    Adds core Git functionalities including repository creation,
    blob creation, index updates, tree creation, and tree indexing.
    
    Implements SHA-1 hashing for content addressing.
    Introduces methods for creating and managing blobs, trees, and the index.
    
    The index is updated to track file hashes and paths.
    Tree structures are created recursively to represent directory contents.
    A working list is used to construct tree structures from the index.
    
    DOES NOT WORK. NEED TO COME BACK LATER. I spent a lot of time on this though.

--- Git.java
+++ Git.java
@@ -8,17 +8,22 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Git {
     public static void main(String[] args) throws FileNotFoundException {
 
     }
+
+    static HashMap<String, String> hash = new HashMap<>();
 
     public static void createRepository() {
         File git = new File("git");
@@ -65,7 +70,7 @@ public class Git {
 
     public static void createBlob(String filePath) throws FileNotFoundException {
         if (filePath == null) {
-            ;
+            return;
         }
         try {
             byte[] bytes = Files.readAllBytes(Paths.get(filePath));
@@ -74,7 +79,8 @@ public class Git {
             File file = new File("git/objects/" + name);
             String s = new String();
             file.createNewFile();
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            hash.put(name, getFilePath(filePath));
+            BufferedReader br = new BufferedReader(new FileReader(getFilePath(filePath)));
             while (br.ready()) {
                 s = s + (br.readLine());
             }
@@ -82,6 +88,7 @@ public class Git {
             BufferedWriter wr = new BufferedWriter(new FileWriter("git/objects/" + name));
             wr.write(s);
             wr.close();
+            updateIndex(filePath);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -94,13 +101,21 @@ public class Git {
         String data = new String(bytes, StandardCharsets.UTF_8);
         String name = hashFunction(data);
         File index = new File("git/index");
+        index.delete();
+        index.createNewFile();
         boolean check = index.exists() && index.length() > 0;
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(index, true));
             if (check == true) {
-                bw.newLine();
+                bw.write('\n');;
             }
-            bw.write(name + " " + filePath);
+            for (Map.Entry<String, String> entry : hash.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                bw.write(value + " " + key);
+
+            }
+
             bw.close();
 
         } catch (IOException e) {
@@ -108,70 +123,201 @@ public class Git {
         }
     }
 
+    public static String getFilePath(String filePath) {
+        File file = new File(filePath);
+        Path base = Paths.get(new File("..").getAbsolutePath());
+        Path relative = Paths.get(file.getAbsolutePath());
+        Path path = base.relativize(relative);
+        return path + "";
+    }
+
     public static String createTree(String directoryPath) throws IOException {
         File directory = new File(directoryPath);
         if (!directory.isDirectory()) {
             throw new IOException("directory path does not lead to a directory");
         }
-        StringBuilder data = new StringBuilder();
         File[] children = directory.listFiles();
-        if (children.length > 0 && children != null) {
+        List<String> entries = new ArrayList<>();
+        if (children != null && children.length > 0) {
+
             for (File child : children) {
                 if (child.isFile()) {
                     createBlob(child.getPath());
-                    String sha = hash.get(getFilePath(child.getPath()));
+                    byte[] bytes = Files.readAllBytes(Paths.get(child.getPath()));
+                    String name = new String(bytes, StandardCharsets.UTF_8);
+                    String sha = hashFunction(name);
                     if (sha != null) {
-                        data.append("blob " + sha + " " + child.getName() + "\n");
+                        entries.add("blob " + sha + " " + child.getName());
                     }
-                    // createBlob(Git.getFilePath(directoryPath + "/" + child));
-                    // byte[] bytes = Files.readAllBytes(Paths.get(directoryPath + "/" + child));
-                    // String data = new String(bytes, StandardCharsets.UTF_8);
-                    // String name = hashFunction(data);
-                    // BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
-                    // bw.write("blob " + name + " " + getFilePath(directoryPath + "/" + child));
                 } else {
-                    String sha = createTree(child.getPath());
-                    data.append("tree " + sha + " " + child.getName() + "\n");
+                    String sha = createTree(child.getPath()); // directoryPath + "/" + child
+                    entries.add("tree " + sha + " " + child.getName());
 
                 }
 
             }
 
         }
-        String treeInfo = data.toString();
+        String treeInfo = String.join("\n", entries);
         String treeHash = hashFunction(treeInfo);
-        File tree = new File("git/objects/" + treeHash);
-        if (!tree.exists()) {
-            tree.createNewFile();
+        Path tree = Paths.get("git", "objects", treeHash);
+        if (!Files.exists(tree)) {
+            Files.write(tree, treeInfo.getBytes(StandardCharsets.UTF_8));
 
         }
-        BufferedWriter bw = new BufferedWriter(new FileWriter(tree));
-        bw.write(treeInfo);
-        bw.close();
 
         return treeHash;
     }
 
 
+
     public static String createIndexTree() throws IOException {
         File index = new File("git/index");
-        File workingList = new File("git/workingList");
-        ArrayList<String> newLines = new ArrayList<>();
-        for (String line : Files.readAllLines(Paths.get("git/index"))) {
-            String[] parts = line.split(" ");
-            String hash = parts[0];
-            String filePath = parts[1];
-            newLines.add("blob " + hash + " " + filePath);
-        }
-        Collections.sort(newLines);
-        Files.write(Paths.get("git/workingList"), newLines);
-
         if (!index.exists()) {
             throw new IOException("index file not found");
         }
+        File workingList = new File("git/objects/workingList");
+        if (!workingList.exists()) {
+            workingList.createNewFile();
+        }
+        ArrayList<String> newLines = new ArrayList<>();
+        for (String line : Files.readAllLines(index.toPath(), StandardCharsets.UTF_8)) {
+            String[] parts = line.split(" ");
+            String hash = parts[0];
+            String filePath = parts[1];
+            newLines.add("blob " + hash + " " + getFilePath(filePath));
+        }
+
+        // if (!newLines.isEmpty()) {
+        // int lastIndex = newLines.size() - 1;
+        // String s = newLines.get(lastIndex);
+        // if (s.endsWith("\n")) {
+        // newLines.set(lastIndex, s.substring(0, s.length() - 1));
+        // }
+        // }
+        Collections.sort(newLines);
+        Files.write(workingList.toPath(), newLines, StandardCharsets.UTF_8);
+        while (Files.readAllLines(Paths.get(workingList.getPath())).size() > 1) {
+            createIndexTreeHelper();
+        }
+        List<String> root = Files.readAllLines(workingList.toPath(), StandardCharsets.UTF_8);
+        if (root.size() == 1) {
+            String[] partitions = root.get(0).split(" ", 3);
+            if (!partitions[0].equals("tree")) {
+                throw new IOException("Splitting did not work");
+            }
+            // String rootHash = partitions[1];
+            // Path mainDir = Paths.get("git", rootHash);
+            // if (!Files.exists(mainDir)) {
+            // Files.write(mainDir, rootHash.getBytes(StandardCharsets.UTF_8));
+            // return rootHash;
+            // }
+            return partitions[1];
+        }
+        return null;
+
 
     }
 
+    private static void createIndexTreeHelper() throws IOException {
+        File workingList = new File("git/objects/workingList");
+        List<String> lines = Files.readAllLines(workingList.toPath(), StandardCharsets.UTF_8);
+        if (lines.isEmpty()) {
+            return;
+        }
+        int maxSlashes = -1;
+        String lowestLine = null;
+        for (String line : lines) {
+            String[] parts = line.split(" ", 3);
+            String pathString = parts[2];
+            char[] chars = pathString.toCharArray();
+            int counter = 0;
+            for (char c : chars) {
+                if (c == '/') {
+                    counter++;
+                }
+            }
+            if (counter > maxSlashes) {
+                maxSlashes = counter;
+                lowestLine = line;
+            }
+            if (lowestLine == null) {
+                return;
+            }
+        }
 
+        String[] parts = lowestLine.split(" ", 3);
+        String type = parts[0];
+        File lowestFile = new File(parts[2]);
+        File targetDirectory = lowestFile.getParentFile();
+        if ("blob".equals(type)) {
+            targetDirectory = lowestFile.getParentFile();
+        } else {
+            targetDirectory = lowestFile;
+        }
+        if (targetDirectory == null || !targetDirectory.isDirectory()) {
+            return;
+        }
+        String treeHash = createTree(targetDirectory.getPath());
+        ArrayList<String> keep = new ArrayList<>();
+        for (String line : lines) {
+            String[] splits = line.split(" ", 3);
+            File file = new File(splits[2]);
+            if (file.getParentFile() == null || !file.getParentFile().equals(targetDirectory)) {
+                keep.add(line);
+            }
+        }
 
+        keep.add("tree " + treeHash + " " + getFilePath(targetDirectory.getPath()));
+        Collections.sort(keep);
+        Files.write(Paths.get("git/objects/workingList"), keep, StandardCharsets.UTF_8);
+        // try {
+        // BufferedReader br = new BufferedReader(new FileReader(workingList));
+        // String line = "";
+        // while (br.ready()) {
+        // line = br.readLine();
+        // lines.add(line);
+        // }
+        // br.close();
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+
+        // String[] originalLines = lines.toArray(new String[0]);
+        // workingList.delete();
+        // workingList.createNewFile();
+        // BufferedWriter bw = new BufferedWriter(new FileWriter(workingList, true));
+        // for (String line : originalLines) {
+        // String[] partitions = line.split(" ");
+        // String path = partitions[2];
+        // File file = new File(path);
+        // if (!file.getParentFile().equals(lowestFile.getParentFile())) {
+        // bw.write(line);
+        // }
+        // }
+        // byte[] content = Files.readAllBytes(Paths.get(workingList.getPath()));
+        // if (content.length > 0 && content[content.length - 1] == '\n') {
+        // byte[] newContent = new byte[content.length - 1];
+        // System.arraycopy(content, 0, newContent, 0, content.length - 1);
+        // Files.write(Paths.get(workingList.getPath()), newContent);
+        // }
+        // bw.write("tree " + treeHash + " " + getFilePath(lowestFile.getParentFile().getPath()));
+        // ArrayList<String> newLines = new ArrayList<>();
+        // for (String line : Files.readAllLines(Paths.get(workingList.getPath()))) {
+        // String[] splits = line.split(" ");
+        // String form = splits[0];
+        // String hash = splits[1];
+        // String relPath = splits[2];
+        // newLines.add(form + " " + hash + " " + relPath + "\n");
+        // }
+        // if (!newLines.isEmpty()) {
+        // int lastIndex = newLines.size() - 1;
+        // String s = newLines.get(lastIndex);
+        // if (s.endsWith("\n")) {
+        // newLines.set(lastIndex, s.substring(0, s.length() - 1));
+        // }
+        // }
+        // Collections.sort(newLines);
+        // bw.close();
+    }
 }
