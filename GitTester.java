9f431a69d7fbc2831047ad1917ddfbc388515167
Cooper Ren <cren2@hwemail.com>
2025-10-08 14:40:17 -0700

    Refactors Git tree creation process
    Simplifies the process of creating Git trees by consolidating file hashing and tree construction into more modular functions. This change removes direct file writing operations in favor of leveraging the Git utility class, improving code organization and maintainability.
    The sample file is also removed.

--- GitTester.java
+++ GitTester.java
@@ -189,18 +189,10 @@ public class GitTester {
         String bContent = "world\n";
         Files.write(a.toPath(), aContent.getBytes(StandardCharsets.UTF_8));
         Files.write(b.toPath(), bContent.getBytes(StandardCharsets.UTF_8));
-        File git = new File("git");
-        File objects = new File("git/objects");
-        objects.mkdirs();
-        String aHash = Git.hashFunction(aContent);
-        String bHash = Git.hashFunction(bContent);
-        String indexLines =
-                aHash + " " + Git.getFilePath(a) + "\n" + bHash + " " + Git.getFilePath(b);
-        Files.write(index.toPath(), indexLines.getBytes(StandardCharsets.UTF_8));
+        Git.createBlob(a.getPath());
+        Git.createBlob(b.getPath());
         String rootTree = Git.createIndexTree();
         System.out.println("Root tree hash: " + rootTree);
-        Path workingList = new File("git/objects/workingList").toPath();
-        System.out.println("workingList");
         Path rootTreePath = new File("git/objects/" + rootTree).toPath();
         if (Files.exists(rootTreePath)) {
             System.out.println("root tree");
